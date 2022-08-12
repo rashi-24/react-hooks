d27738133a6963ebe3579493edc60092df06d44a
rashi-24 <sharma.rashi2408@gmail.com>
2022-08-12 21:24:44 +0530

    rashi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import First from './components/First';
+import Five from './components/Five';
+import Four from './components/Four';
+import Second from './components/Second';
+import Three from './components/Three';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <First />
+      <Second />
+      <Three />
+      <Four />
+      <Five />
     </div>
   );
 }
